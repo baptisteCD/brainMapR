e8fb91d9e72b6a556f8ec692f66898cd1cd48c81
Baptiste Couvy-Duchesne <baptiste.couvyduchesne@gmail.com>
2021-10-06 19:03:25 +0200

    input - output folders in functions

--- R/plots.R
+++ R/plots.R
@@ -3,15 +3,16 @@
 #' This function takes in raw brain association maps, formats it and writes plot
 #'
 #'
-#' @param path path (folder) to the raw brain association maps (outputs of OSCA)
+#' @param inputPath path (folder) to the raw brain association maps (outputs of OSCA)
 #' @param bwasFile name of the brain association map
 #' @param yMax maximum of the y-axis
 #' @param phenotypeLabel Label of the phenotype - for plotting
 #' @param signifThreshold significance threshold
+#' @param outputPath path where the outputs will be written
 #' @return Manhattan plot as well as formatted brain association map (full and subsetted for significant vertices)
 #' @import plyr png qqman readr Rvcg rgl RColorBrewer grid gridExtra viridis Morpho ggplot2 utils stats graphics grDevices
 #' @export
-BrainMapManhattanPlot<-function(path , bwasFile , yMax, phenotypeLabel, signifThreshold){
+BrainMapManhattanPlot<-function(inputPath , bwasFile , yMax, phenotypeLabel, signifThreshold, outputPath){
 
 BWASsignif=NULL
 chi2All=NULL
@@ -20,11 +21,11 @@ lambda=NULL
 signifLog10= -log( signifThreshold, base=10 )
 
 # Initialise plot
-png(paste0(path, "Manhathan_", bwasFile, "_simple.png"), width = 50, height = 16, units = "cm", res = 400, type="cairo")
+png(paste0(inputPath, "Manhathan_", bwasFile, "_simple.png"), width = 50, height = 16, units = "cm", res = 400, type="cairo")
 plot.new()
 layout(matrix(c(1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,6,7,8), nrow =1 , ncol = 20, byrow = F))
 
-BWASsumstatfilePath = paste0(path ,  bwasFile)
+BWASsumstatfilePath = paste0(inputPath ,  bwasFile)
 BWASsumstatfile<-  read.table(BWASsumstatfilePath, header=T, stringsAsFactors = F)
 BWASsumstatfile$log10p<- -log10(BWASsumstatfile$p)
 
@@ -108,8 +109,8 @@ abline(h= signifLog10 , col="darkred", lwd=2)
 }
 dev.off()
 
-write.csv(BWASsignif, paste0(path, "/BWAS_fullSummary_", phenotypeLabel, ".csv" ))
-write.csv(BWASsignif[which(BWASsignif$log10p > signifLog10),], paste0(path, "/BWAS_signif_", phenotypeLabel , ".csv" ))
+write.csv(BWASsignif, paste0(outputPath, "/BWAS_fullSummary_", bwasFile, ".csv" ))
+write.csv(BWASsignif[which(BWASsignif$log10p > signifLog10),], paste0(outputPath, "/BWAS_signif_", bwasFile , ".csv" ))
 
 }
 
@@ -149,19 +150,20 @@ expectedP <- -log10(ppoints(length(pvector)))
 #' This function reads in a list of brain association maps, which have been formatted and created as part of the Manhattan plot creation (this allows inferring more easily the file names)
 #'
 #'
-#' @param path path to working directory
+#' @param inputPath path to working directory containing the outputs of BrainMapManhattanPlot()
 #' @param scenarioList List of scenarios/folders each containing a brain association map
 #' @param legendList List of labels for the different scenarios/folders
 #' @param variableLabel label of the variable
 #' @param colourList List of colours for the different scenarios/folders
+#' @param outputPath path where the outputs will be written
 #' @return QQplot with several distributions
 #' @export
-superimposedQQplot=function(path , scenarioList,  legendList, colourList, variableLabel){
+superimposedQQplot=function(inputPath , scenarioList,  legendList, colourList, variableLabel, outputPath){
 
 jjj=2
-png(paste0(path, "QQplotCombined_assocVertices", variableLabel, ".png"), width = 15, height = 15, units = "cm", res=400)
+png(paste0(outputPath, "QQplotCombined_assocVertices", variableLabel, ".png"), width = 15, height = 15, units = "cm", res=400)
 par(mar=c(4,4,2,1))
-bwas=read_csv(paste0(path, scenarioList[1], "/BWAS_fullSummary_", variableLabel, ".csv"))
+bwas=read_csv(paste0(inputPath, scenarioList[1], "/BWAS_fullSummary_", variableLabel, ".csv"))
 qqplot(bwas$p, col=colourList[1])
  print(round(median(bwas$CHI2,na.rm=T)/qchisq(0.5,df=1),3))
 legend(x = 3.5, y = 0.75*max(bwas$log10p), legend = legendList ,pch=20, pt.cex=1.5,  col = colourList)
@@ -184,16 +186,17 @@ dev.off()
 #' The function opens the phenotype file in order to calculate the variance of the phenotype, used to convert association betas into correlation coefficients.
 #'
 #'
-#' @param path path to working directory (input and output folder)
+#' @param inputPath path (folder) to the raw brain association maps (outputs of identifyClustersBWAS())
 #' @param bwasFile name of the brain association map (without extension)
 #' @param pathPhenotypeFile Path to the variable files (e.g. variable.phen) used get variance of the phenotype
 #' @return Outside and Inside snapshots of the surfaces.
+#' @param outputPath path where the outputs will be written
 #' @import plyr png qqman readr Rvcg rgl RColorBrewer grid gridExtra viridis Morpho ggplot2 utils stats graphics grDevices
 #' @export
-plotSubcortical=function(path, bwasFile, pathPhenotypeFile){
+plotSubcortical=function(inputPath, bwasFile, pathPhenotypeFile, outputPath){
   for (moda in c("thick", "LogJacs")){
  for (hemi in c("lh", "rh")){
-bwasPlot=read.table( paste0(path, bwasFile , "_", moda, "_clustersAndCoordinates"), header=T)
+bwasPlot=read.table( paste0(inputPath, bwasFile , "_", moda, "_clustersAndCoordinates"), header=T)
 bwasPlot=bwasPlot[which(bwasPlot$hemi==hemi),]
 bwasPlot$X=bwasPlot$X*(-1)
 bwasPlot$Y=bwasPlot$Y*(-1)
@@ -216,14 +219,14 @@ bwasPlot$radius=ifelse(  bwasPlot$signifVoxel==1, 1.5 ,0.8 )
 # Draw plots and save screenshots
 par3d(windowRect = c(0, 0, 800, 800)*1.5, zoom=0.8)
 spheres3d(as.matrix(bwasPlot[,c( "Z",  "X", "Y")]), col=bwasPlot$color, radius = bwasPlot$radius)
-rgl.snapshot(paste0(path, "/BWAS_", bwasFile, "_", hemi, "_", moda , "_clustersAndCoordinates_inside.png"))
+rgl.snapshot(paste0(outputPath, "/BWAS_", bwasFile, "_", hemi, "_", moda , "_clustersAndCoordinates_inside.png"))
 rgl.close()
 
 bwasPlot$X=bwasPlot$X*(-1)
 bwasPlot$Z=bwasPlot$Z*(-1)
 par3d(windowRect = c(0, 0, 800, 800)*1.5, zoom=0.8)
 spheres3d(as.matrix(bwasPlot[,c( "Z",  "X", "Y")]), col=bwasPlot$color, radius = bwasPlot$radius)
-rgl.snapshot(paste0(path, "/BWAS_", bwasFile,"_", hemi, "_", moda , "_clustersAndCoordinates_outside.png"))
+rgl.snapshot(paste0(outputPath, "/BWAS_", bwasFile,"_", hemi, "_", moda , "_clustersAndCoordinates_outside.png"))
 rgl.close()
 
 }}
@@ -236,17 +239,17 @@ rgl.close()
 #' The function opens the phenotype file in order to calculate the variance of the phenotype, used to convert association betas into correlation coefficients.
 #'
 #'
-#' @param path path to working directory (input and output folder)
+#' @param inputPath path (folder) to the brain association maps (outputs of identifyClustersBWAS())
 #' @param bwasFile name of the brain association map (without extension)
 #' @param pathPhenotypeFile Path to the variable files (e.g. variable.phen) used get variance of the phenotype
-#' @return Outside and Inside snapshots of the surfaces - the subcortical structures are spread out to faciliate viewing.
+#' @return Outside and Inside snapshots of the surfaces.
+#' @param outputPath path where the outputs will be written
 #' @import plyr png qqman readr Rvcg rgl RColorBrewer grid gridExtra viridis Morpho ggplot2 utils stats graphics grDevices
-#' @export
-plotSubcortical_flat=function(path, bwasFile, pathPhenotypeFile){
+plotSubcortical_flat=function(inputPath, bwasFile, pathPhenotypeFile, outputPath){
 
   for (moda in c("thick", "LogJacs")){
  for (hemi in c("lh", "rh")){
-bwasPlot=read.table(paste0(path, bwasFile , "_", moda, "_clustersAndCoordinates"), header=T)
+bwasPlot=read.table(paste0(inputPath, bwasFile , "_", moda, "_clustersAndCoordinates"), header=T)
 bwasPlot=bwasPlot[which(bwasPlot$hemi==hemi),]
 bwasPlot$X=bwasPlot$X*(-1)
 bwasPlot$Y=bwasPlot$Y*(-1)
@@ -366,12 +369,12 @@ bwasPlot$Zf2[bwasPlot$subcv %in% c(10,49)]=bwasPlot$Zf2[bwasPlot$subcv %in% c(10
 # Draw plots and save screenshots
 par3d(windowRect = c(0, 0, 800, 800)*1.5, zoom=0.8)
 spheres3d(as.matrix(bwasPlot[,c( "Zf",  "Xf", "Yf")]), col=bwasPlot$color, radius = bwasPlot$radius)
-rgl.snapshot(paste0(path, "/BWAS_", bwasFile, "_", hemi, "_", moda , "_clustersAndCoordinates_inside.png"))
+rgl.snapshot(paste0(outputPath, "/BWAS_", bwasFile, "_", hemi, "_", moda , "_clustersAndCoordinates_inside.png"))
 rgl.close()
 
 par3d(windowRect = c(0, 0, 800, 800)*1.5, zoom=0.8)
 spheres3d(as.matrix(bwasPlot[,c( "Zf2",  "Xf2", "Yf2")]), col=bwasPlot$color, radius = bwasPlot$radius)
-rgl.snapshot(paste0(path, "/BWAS_", bwasFile,"_", hemi, "_", moda , "_clustersAndCoordinates_outside.png"))
+rgl.snapshot(paste0(outputPath, "/BWAS_", bwasFile,"_", hemi, "_", moda , "_clustersAndCoordinates_outside.png"))
 rgl.close()
 
 }}
@@ -384,18 +387,19 @@ rgl.close()
 #' The function opens the phenotype file in order to calculate the variance of the phenotype, used to convert association betas into correlation coefficients.
 #'
 #'
-#' @param path path to working directory (input and output folder)
+#' @param inputPath path (folder) to the brain association maps (outputs of identifyClustersBWAS())
 #' @param bwasFile name of the brain association map (without extension)
 #' @param pathPhenotypeFile Path to the variable files (e.g. variable.phen) used get variance of the phenotype
+#' @param outputPath path where the outputs will be written
 #' @return Outside and Inside snapshots of the surfaces.
 #' @import plyr png qqman readr Rvcg rgl RColorBrewer grid gridExtra viridis Morpho ggplot2 utils stats graphics grDevices
 #' @export
-plotCortical=function(path, bwasFile, pathPhenotypeFile){
+plotCortical=function(inputPath, bwasFile, pathPhenotypeFile, outputPath){
 
  for (moda in c("area", "thickness")){
 
  for (hemi in c("rh", "lh")){
-bwasPlot=read.table(paste0(path, bwasFile , "_", moda, "_clustersAndCoordinates"), header=T)
+bwasPlot=read.table(paste0(inputPath, bwasFile , "_", moda, "_clustersAndCoordinates"), header=T)
 bwasPlot=bwasPlot[which(bwasPlot$hemi==hemi),]
 
 # Open phenotype file
@@ -421,14 +425,14 @@ bwasPlot2=bwasPlot
 # Draw plot
 par3d(windowRect = c(0, 0, 800, 800)*1.5, zoom=0.8)
 spheres3d(as.matrix(bwasPlot2[,c( "Y","X", "Z")]), col=bwasPlot2$color, radius = bwasPlot2$radius)
-rgl.snapshot(paste0(path, "/BWAS_", bwasFile,"_", hemi, "_", moda , "_clustersAndCoordinates_inside.png"))
+rgl.snapshot(paste0(outputPath, "/BWAS_", bwasFile,"_", hemi, "_", moda , "_clustersAndCoordinates_inside.png"))
 rgl.close()
 
 bwasPlot2$X=bwasPlot2$X*(-1)
 bwasPlot2$Y=bwasPlot2$Y*(-1)
 par3d(windowRect = c(0, 0, 800, 800)*1.5, zoom=0.8)
 spheres3d(as.matrix(bwasPlot2[,c( "Y","X", "Z")]), col=bwasPlot2$color, radius = bwasPlot2$radius)
-rgl.snapshot(paste0(path, "/BWAS_", bwasFile,"_", hemi, "_", moda , "_clustersAndCoordinates_outside.png"))
+rgl.snapshot(paste0(outputPath, "/BWAS_", bwasFile,"_", hemi, "_", moda , "_clustersAndCoordinates_outside.png"))
 rgl.close()
 
 }}
@@ -440,21 +444,22 @@ rgl.close()
 #' This function uses the cortical ans subcortical figures created using the specific functions
 #'
 #'
-#' @param path path to working directory (input and output folder)
+#' @param inputPath path (folder) to the cortical and subcortical plots
 #' @param bwasFile Variable name (used to name the clustersAndCoordinates file)
 #' @param pathToLegendBar Path to the manually created legend bar
+#' @param outputPath path where the outputs will be written
 #' @return Outside and Inside snapshots of the surfaces.
 #' @import plyr png qqman readr Rvcg rgl RColorBrewer grid gridExtra viridis Morpho ggplot2 utils stats graphics grDevices
 #' @export
-combineCorticalSubcorticalPlots=function(path, bwasFile, pathToLegendBar){
+combineCorticalSubcorticalPlots=function(inputPath, bwasFile, pathToLegendBar, outputPath){
 
 # List of files
 ll=NULL
 for (moda in c("thickness", "area","thick", "LogJacs") ){
-ll=c(ll, paste0(path, "/BWAS_", bwasFile, "_", "lh", "_", moda, "_clustersAndCoordinates_outside.png"))
-ll=c(ll, paste0(path, "/BWAS_", bwasFile, "_","lh", "_", moda, "_clustersAndCoordinates_inside.png"))
-ll=c(ll, paste0(path, "/BWAS_", bwasFile, "_", "rh", "_", moda, "_clustersAndCoordinates_inside.png"))
-ll=c(ll, paste0(path, "/BWAS_", bwasFile, "_", "rh", "_", moda, "_clustersAndCoordinates_outside.png"))
+ll=c(ll, paste0(inputPath, "/BWAS_", bwasFile, "_", "lh", "_", moda, "_clustersAndCoordinates_outside.png"))
+ll=c(ll, paste0(inputPath, "/BWAS_", bwasFile, "_","lh", "_", moda, "_clustersAndCoordinates_inside.png"))
+ll=c(ll, paste0(inputPath, "/BWAS_", bwasFile, "_", "rh", "_", moda, "_clustersAndCoordinates_inside.png"))
+ll=c(ll, paste0(inputPath, "/BWAS_", bwasFile, "_", "rh", "_", moda, "_clustersAndCoordinates_outside.png"))
 }
 ll=c(ll, paste0(pathToLegendBar))
 
@@ -474,8 +479,7 @@ lay <- rbind(c(1,1,3,3,5,5,7,7,9,9,11,11,13,13,15,15,17),
             c(2,2,4,4,6,6,8,8,10,10,12,12,14,14,16,16, 17))
 
 gs=grid.arrange(grobs = plots2, layout_matrix = lay)
-ggsave(paste0(path, "/Plots_Combined", bwasFile, ".png"),width=18, height=4, gs)
-
+ggsave(paste0(outputPath, "/Plots_Combined", bwasFile, ".png"),width=18, height=4, gs)
 
 }
 
